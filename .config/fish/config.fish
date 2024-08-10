7825fcd6801d82c2db3bf437f17cec692485ffb1
miRceLzeS <mczinshort@gmail.com>
2024-08-10 21:06:26 +0800

    New Base

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-if not pgrep -u (whoami) ssh-agent > /dev/null
-  eval (ssh-agent -c)
-  ssh-add ~/.ssh/id_rsa
-end
