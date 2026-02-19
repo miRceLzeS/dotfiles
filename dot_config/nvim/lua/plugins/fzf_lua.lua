326254d7e3c63bee20d2713d490b9449d61e5240
mircelzes <mircelzes727@gmail.com>
2026-02-19 13:36:18 +0800

    feat(nvim): fzf-lua file picker on VimEnter

--- dot_config/nvim/lua/plugins/fzf_lua.lua
+++ dot_config/nvim/lua/plugins/fzf_lua.lua
@@ -1,7 +1,7 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
+  lazy = false,
   opts = {
     { "telescope", "hide" },
     hls = {
@@ -31,6 +31,8 @@ return {
     local function leader(s)
       return "<leader>" .. s
     end
+    map({"n"}, leader("b"), function() fzf.buffers() end)
+    map({"n"}, leader("d"), function() fzf.diagnostics_workspace() end)
     map({"n"}, leader("ff"), function()
       fzf.files({ cwd = vim.fn.expand("%:p:h") })
     end)
@@ -38,8 +40,27 @@ return {
       fzf.files({ cwd = vim.fn.stdpath("config") })
     end)
     map({"n"}, leader("fF"), function() fzf.files() end)
-    map({"n"}, leader("b"), function() fzf.buffers() end)
     map({"n"}, leader("lg"), function() fzf.live_grep() end)
-    map({"n"}, leader("d"), function() fzf.diagnostics_workspace() end)
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        local argc = vim.fn.argc()
+        local open_picker = false
+        if argc == 0 then
+          open_picker = true
+        elseif argc == 1 then
+          local arg0 = vim.fn.argv(0)
+          if vim.fn.isdirectory(arg0) == 1 then
+            open_picker = true
+            cwd = arg0
+          end
+        end
+        if (not open_picker) or (vim.bo.buftype ~= "") then
+          return
+        end
+        vim.schedule(function()
+          fzf.files(cwd and { cwd = cwd } or nil)
+        end)
+      end
+    })
   end,
 }
