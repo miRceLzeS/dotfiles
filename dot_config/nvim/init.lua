a4f0fe2f51ea617c84fc9faf9b9328d2233d6829
miRceLzeS <mircelzes727@gmail.com>
2026-03-29 16:57:10 +0800

    feat: optimize neovim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,40 +1,55 @@
 -- [INFO] global
 local G = vim.g
+
 G.mapleader = " "
 G.maplocalleader = "\\"
 
 -- [INFO] option
 local opt = vim.opt
--- appearance
+
+-- visual effect
 opt.colorcolumn = "64"
 opt.cursorline = true
 opt.inccommand = "split"
-opt.listchars = { tab = "▎ ", trail = "·", nbsp = "␣" }
-opt.list = true
-opt.number = true
-opt.relativenumber = true
+opt.jumpoptions = "clean,view"
+opt.laststatus = 3 -- global status line
 opt.termguicolors = true
 opt.showmode = false
 opt.scrolloff = 4
 opt.signcolumn = "yes"
+opt.shortmess:append("aIc")
 opt.winborder = "rounded"
+
+opt.listchars = { tab = "▎ ", trail = "·", nbsp = "␣" }
+opt.list = true
+
+opt.number = true
+opt.relativenumber = true
+
+-- split
+opt.splitbelow = true
+opt.splitright = true
+
 -- wrap
 opt.wrap = false
 opt.linebreak = true -- line will break at suitable character if true
 opt.breakindent = true -- the new visual line will be indented
 opt.showbreak = "≪ "
+
 -- indent
 opt.expandtab = true -- number of spaces will be inserted replacing a tab
 local tab_spaces = 2
 opt.tabstop = tab_spaces -- number of visual spaces of a tab
 opt.shiftwidth = tab_spaces -- number of spaces when auto indent
 opt.softtabstop = tab_spaces -- number of spaces of cursor's movement
+
 -- search: ignore case unless upper case character is typed
 opt.ignorecase = true
 opt.smartcase = true
+
 -- io
 opt.autoread = true
-opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+opt.autowrite = true
 opt.confirm = true
 opt.mouse = "nv"
 opt.swapfile = false
@@ -42,10 +57,16 @@ opt.timeoutlen = 512
 opt.undofile = true
 opt.updatetime = 256
 
+-- motion
+opt.iskeyword:append("_", "-", ".")
+
 -- [INFO] keymap
+
 -- keymap util
+
 function map(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
+
   if opts then
     options = vim.tbl_extend("force", options, opts)
   end
@@ -56,56 +77,113 @@ function unmap(mode, lhs, opts)
   map(mode, lhs, "<Nop>", opts)
 end
 
-function cleancmd()
-  vim.api.nvim_echo({ { "" } }, false, {})
-  vim.cmd("redraw")
+function op_motion_expr(op, motion)
+  if not op then return "" end
+
+  if motion == "h" then
+    return string.format("leftabove v%s", op)
+  elseif motion == "l" then
+    return string.format("rightbelow v%s", op)
+  elseif motion == "k" then
+    return string.format("aboveleft %s", op)
+  elseif motion == "j" then
+    return string.format("%s", op)
+  else return ""
+  end
 end
+
+-- unmap
+
+unmap({ "n", "v" }, "s")
+
 -- enhance
+
 map("n", "<Esc>", "<Cmd>nohlsearch<CR>")
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+
+map("x", "<", "<gv")
+map("x", ">", ">gv")
+
 map("n", "<C-u>", "<C-u>zz")
 map("n", "<C-d>", "<C-d>zz")
-map({ "n", "v" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-map({ "n", "v" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
-map({ "n", "v" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-map({ "n", "v" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev search result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+
 -- customize
+
 map("n", "U", "<C-r>")
-map({ "n", "v" }, "gh", "0")
-map({ "n", "v" }, "gl", "$")
-map("n", "<M-u>", "<Cmd>m .-2<CR>==", { desc = "move current line up" })
-map("n", "<M-d>", "<Cmd>m .+1<CR>==", { desc = "move current line down" })
-map("v", "<M-u>", ":m '<-2<CR>gv=gv", { desc = "move current line up" })
-map("v", "<M-d>", ":m '>+1<CR>gv=gv", { desc = "move current line down" })
-map("i", "<M-u>", "<Esc><Cmd>m .-2<CR>==gi", { desc = "move current line up" })
-map("i", "<M-d>", "<Esc><Cmd>m .+1<CR>==gi", { desc = "move current line down" })
+
+map({ "n", "x" }, "gh", "0")
+map({ "n", "x" }, "gl", "$")
+
+map("n", "<M-u>", "<Cmd>m .-2<CR>==", { desc = "Move current line up" })
+map("n", "<M-d>", "<Cmd>m .+1<CR>==", { desc = "Move current line down" })
+map("x", "<M-u>", ":m '<-2<CR>gv=gv", { desc = "Move current line up" })
+map("x", "<M-d>", ":m '>+1<CR>gv=gv", { desc = "Move current line down" })
+map("i", "<M-u>", "<Esc><Cmd>m .-2<CR>==gi", { desc = "Move current line up" })
+map("i", "<M-d>", "<Esc><Cmd>m .+1<CR>==gi", { desc = "Move current line down" })
+
 -- toggle
-map({ "n", "v" }, "<leader>tw", function()
+
+map({ "n", "x" }, "<leader>tw", function()
   local wo = vim.wo
+
   wo.wrap = not wo.wrap
-  -- [TODO] change to autocmd
-  wo.colorcolumn = wo.wrap and "" or "64"
+  wo.colorcolumn = wo.wrap and "" or "64" -- [TODO] change to autocmd
 end, { desc = "toggle wrap" })
+
+-- yank
+map("x", "p", '"_dP')
+map("v", "<Leader>y", '"+y')
+
+-- buffer
+map({ "n", "x" }, "<Leader>b[", "<Cmd>bprevious<CR>", { desc = "Goto prievous buffer" })
+map({ "n", "x" }, "<Leader>b]", "<Cmd>bnext<CR>", { desc = "Goto next buffer" })
+
 -- window
-map({ "n", "v" }, "<leader>wh", "<C-w>h", { desc = "Goto window left" })
-map({ "n", "v" }, "<leader>wl", "<C-w>l", { desc = "Goto window right" })
-map({ "n", "v" }, "<leader>wk", "<C-w>k", { desc = "Goto window up" })
-map({ "n", "v" }, "<leader>wj", "<C-w>j", { desc = "Goto window down" })
-map({ "n", "v" }, "<leader>ws", function()
-  vim.api.nvim_echo({ { "(← h | → l | ↑ k | ↓ j)" } }, false, {})
+map({ "n", "x" }, "<Leader>wh", "<C-w>h", { desc = "Goto window left" })
+map({ "n", "x" }, "<Leader>wl", "<C-w>l", { desc = "Goto window right" })
+map({ "n", "x" }, "<Leader>wk", "<C-w>k", { desc = "Goto window up" })
+map({ "n", "x" }, "<Leader>wj", "<C-w>j", { desc = "Goto window down" })
+
+map({ "n", "x" }, "<Leader>ws", function()
   local ok, key = pcall(vim.fn.getcharstr)
   if not ok then return end
-  if key == "h" then
-    vim.cmd("leftabove vsplit")
-  elseif key == "l" then
-    vim.cmd("rightbelow vsplit")
-  elseif key == "k" then
-    vim.cmd("aboveleft split")
-  elseif key == "j" then
-    vim.cmd("botright split")
-  else cleancmd() return
-  end
-  vim.cmd("wincmd " .. key)
-  cleancmd()
-end, { desc = "split window to direction" })
+
+  local expr = op_motion_expr("split", key)
+  if expr == "" then return end
+
+  vim.cmd(expr)
+end, { desc = "Split window to direction" })
+
+map({ "n", "x" }, "<Leader>wn", function()
+  local ok, key = pcall(vim.fn.getcharstr)
+  if not ok then return end
+
+  local expr = op_motion_expr("new", key)
+  if expr == "" then return end
+
+  vim.cmd(expr)
+end, { desc = "Create new window at direction" })
+
+map({ "n", "x" }, "<Leader>wo", "<Cmd>only<CR>", { desc = "Close other windows" })
+
+-- tab
+map({ "n", "x" }, "<Leader><Tab>[", "<Cmd>tabprevious<CR>")
+map({ "n", "x" }, "<Leader><Tab>]", "<Cmd>tabnext<CR>")
+
+map({ "n", "x" }, "<Leader><Tab>n", "<Cmd>tabnew<CR>")
+
+map({ "n", "x" }, "<Leader><Tab>o", "<Cmd>tabonly<CR>", { desc = "Close other tabs" })
+
+-- file
+map({ "n", "x" }, "<Leader>fn", "<Cmd>enew<CR>", { desc = "Create new file" })
